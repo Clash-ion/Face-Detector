31bf03932e7da76b294ca249915327affc543400
Mukul Kumar <mukul0000kumar@gmail.com>
2022-08-17 12:12:32 +0530

    Add Files

---
+++ test.py
@@ -0,0 +1,10 @@
+# A function that calculates fibonacci numbers
+def fibonacci(n):
+    if n == 0:
+        return 0
+    elif n == 1:
+        return 1
+    else:
+        return fibonacci(n-1) + fibonacci(n-2)
+
+print(fibonacci(10))
